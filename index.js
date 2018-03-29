559e5c18262b2762c120f27e304039d7de7c4cb0
piotrkulak@outlook.com <piotrkulak@outlook.com>
2018-03-29 22:27:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,14 @@ function nowServing(line) {
   }
   return `Currently serving ${line.shift()}.`
 }
+
+function currentLine(line) {
+  if (!line.length) {
+    return "The line is currently empty."
+  }
+  const numbersAndNames = []
+  for (let i = 0, l = line.length; i < l; i++) {
+    numbersAndNames.push(`${i + 1}. ${line[i]}`)
+  }
+  return `The line is currently: ${numbersAndNames.join(', ')}`
+}
